8cff1fce3b7daf565e41fefd02c3d9bf518d6a12
Sara Afshar <73103443+afshars@users.noreply.github.com>
2022-04-20 03:43:38 +0430

    Update CompleteNumber.py

--- CompleteNumber.py
+++ CompleteNumber.py
@@ -4,34 +4,40 @@
 #------------------------------------------
 
 
-
+print("----------------start-----------------")
 while True:
-    for num in range (1,500):
-   #num =int(input('plz enter a number :'))  
-        divisors =[]
-        d=1
+   num =int(input('plz enter a number :')) 
 
-        while d<=num :
-            if num % d==0:
-                x= num/d
-                if x!=num:
-                    divisors.append(int(x))
-                d+=1
-            else :
-                d+=1
+   # divisors of  the number
+   divisors =[]
+   d=1
+   while d<=num :
+      if num % d==0:
+         x= num/d
+         if x!=num:
+            divisors.append(int(x))
+         d+=1
+      else :
+         d+=1
 
-        print('divisor :',divisors)       
+   print('divisor :',divisors)       
 
-        sum=0
-        for i in  range (len(divisors)):
-            sum +=divisors[i]
-            i+=1
-        print('sum of divisors :',sum)
-
-        if sum==num :
-            print("The number entered is Perfect Number")
-            
-        answer = input('Do you want to continue? ').lower()
-        if answer=='no':
-            break
-        num+=50
+    # summation of divisors
+   sum=0
+   for i in  range (len(divisors)):
+      sum +=divisors[i]
+      i+=1
+   print('sum of divisors :',sum)
+   
+    # find perfect number
+   if sum==num :
+      print("The number entered is Perfect Number")
+    
+   print("--------------------------------------")
+   
+   # Exit or Continue
+   answer = input('Do you want to continue (y or n) ? ').lower()   
+   if answer=='n':
+        print("----------------finish----------------")
+        break
+   
